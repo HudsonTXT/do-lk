36f83c09965189b6d17ddb95e9e2923f7ac66e01
hudsontxt <livecheg@ya.ru>
2017-07-20 03:35:12 +0500

    song loaded

--- js/dance.js
+++ js/dance.js
@@ -3,5 +3,18 @@ $(function () {
         $('div').removeClass('dance__songs_selected');
         $(this).addClass('dance__songs_selected');
 
-    })
+    });
+
+    $.getJSON('http://fandance.ru/music/events.php?do=getSongs', function (json) {
+        if (json) {
+            for (var i = json.length - 1; i >= 0; i--) {
+                music = '<div class="block_element" data-song-id="' + json[i].id + '">';
+                music += '<div class="block__separator dance__track">' + json[i].author + ' - ' + json[i].name + '</div>';
+                music += '<div class="block__separator dance__bpm">' + json[i].bpm + ' BPM</div>';
+                music += '<div class="block__separator dance__time">' + json[i].length + '</div>';
+                music += '</div>';
+                $('.dance__songs').append(music);
+            }
+        }
+    });
 });

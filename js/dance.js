ceaf486c0190a9197df2ca425a4bc95a7ba9e82a
hudsontxt <livecheg@ya.ru>
2017-07-20 03:41:48 +0500

    clear

--- js/dance.js
+++ js/dance.js
@@ -7,6 +7,7 @@ $(function () {
 
     $.getJSON('https://fandance.ru/music/events.php?do=getSongs', function (json) {
         if (json) {
+            $('.dance__songs').html(' ');
             for (var i = json.length - 1; i >= 0; i--) {
                 music = '<div class="block_element" data-song-id="' + json[i].id + '">';
                 music += '<div class="block__separator dance__track">' + json[i].author + ' - ' + json[i].name + '</div>';

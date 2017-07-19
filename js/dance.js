fe700d39a28af45e18cce74c5d8a352cedb8982c
hudsontxt <livecheg@ya.ru>
2017-07-20 03:36:42 +0500

    http to  https

--- js/dance.js
+++ js/dance.js
@@ -5,7 +5,7 @@ $(function () {
 
     });
 
-    $.getJSON('http://fandance.ru/music/events.php?do=getSongs', function (json) {
+    $.getJSON('https://fandance.ru/music/events.php?do=getSongs', function (json) {
         if (json) {
             for (var i = json.length - 1; i >= 0; i--) {
                 music = '<div class="block_element" data-song-id="' + json[i].id + '">';
